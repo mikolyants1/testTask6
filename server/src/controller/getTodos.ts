be2c3bd14ff2dd39089232ffb5a31dbad60a4d50
mikolyants1 <y.mikolyants@mail.ru>
2024-01-07 16:36:58 +0300

    g

---
+++ server/src/controller/getTodos.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { IPost } from "../type.js";
+import { Post } from "../mongo.js";
+
+
+export async function getTodos(req:Request,res:Response):Promise<void>{
+    const posts:IPost[] = await Post.find();
+    if (!posts){
+      res.status(400).json({
+         mess:"posts not found"
+      });
+      return;
+    };
+    res.status(200).json(posts);
+}
--- server/src/router/router.ts
+++ server/src/router/router.ts
@@ -1,21 +1,18 @@
 import express, { Router } from "express";
-import { getPosts } from "../controller/getPosts.js";
-import { getPost } from "../controller/getPost.js";
+import { getTodos } from "../controller/getTodos.js";
 import { Validator, valid } from "../middlewares/valid.js";
 import { addTodo } from "../controller/addTodo.js";
-import { delPost } from "../controller/delPost.js";
-import { chanPost } from "../controller/chanPost.js";
+import { delTodo } from "../controller/delTodo.js";
+import { chanTodo } from "../controller/chanTodo.js";
 
 const router:Router = express.Router();
 
-router.put('/:id',valid,Validator,chanPost);
+router.put('/:id',valid,Validator,chanTodo);
 
-router.delete('/:id',delPost)
-
-router.get("/:id",getPost);
+router.delete('/:id',delTodo)
 
 router.post('/',valid,Validator,addTodo);
 
-router.get("/",getPosts);
+router.get("/",getTodos);
 
 export default router;
